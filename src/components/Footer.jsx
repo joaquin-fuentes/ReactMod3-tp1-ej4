fad80735bf941dd543ddacf80e681d9ccc5a93b0
Joaquin Fuentes <joaquin.fuentes1327@gmail.com>
2023-05-17 10:19:42 -0300

    se creo el footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className='bg-dark text-center p-4'>
+            <h3 className='text-light'>Todos los derechos reservados</h3>
+        </footer>
+    );
+};
+
+export default Footer;
