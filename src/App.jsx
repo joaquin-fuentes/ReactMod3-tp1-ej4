a08630896add41fb8131e40d7adde3e15c44f2a5
Joaquin Fuentes <joaquin.fuentes1327@gmail.com>
2023-05-17 10:40:02 -0300

    agregamos el maquetado de app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import Footer from './components/Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Container} from "react-bootstrap"
+import './App.css'
+
 
 
 const App = () => {
   return (
     <>
-      
+      <Container className='my-5 mainPage text-light'>
+        <h1 className='display-4 text-center'>Lista de Tareas</h1>
+        <hr/>
+        {/* aqui va el formulario */}
+      </Container>
       <Footer></Footer>
     </>
   );
